faca35eea5e8aa2cd78079a1afe8c7bd2e5b0a7d
Eric Sartor <esartor@uoguelph.ca>
2022-03-27 00:35:23 -0400

    file reading comp1337

--- raytrace.js
+++ raytrace.js
@@ -18,6 +18,7 @@ class Sphere {
 		this.ambient = {r: ar, g: ag, b: ab};
 		this.diffuse = {r: dr, g: dg, b: db};
 		this.specular = {r: sr, g: sg, b: sb};
+		this.shininess = shiny;
 	}
 }
 
@@ -45,21 +46,35 @@ function readFile(input) {
 		const lines = reader.result.split('\n');
 		let sphereCount = 0;
 		if (lines[0]) {
-			let linfo = lines[0].split();
+			console.log(lines[0]);
+			let linfo = lines[0].split(/\s+/);
 			//the following line also garbage-collects the old light, i think
-			light = new Light(linfo[1], linfo[2], linfo[3], linfo[4], linfo[5], linfo[6], linfo[7], linfo[8], linfo[9]);
+			console.log("light info:",linfo[1], linfo[2], linfo[3],	//x y z
+			linfo[4], linfo[5], linfo[6],	//ambient
+			linfo[7], linfo[8], linfo[9]);
+			light = new Light(parseInt(linfo[1]), parseInt(linfo[2]), parseInt(linfo[3]),	//x y z
+							  parseInt(linfo[4]), parseInt(linfo[5]), parseInt(linfo[6]),	//ambient
+							  parseInt(linfo[7]), parseInt(linfo[8]), parseInt(linfo[9]));	//specular
 		} else {console.error("Possible improperly formatted input file");}
 		if (lines[1]) {
+			console.log(lines[1]);
 			sphereCount = parseInt(lines[1].replace('#', '')); //NOTE: wait just realized there will be no # in it LOL
 		} else {console.error("Improper sphere count read in!");}
 		if (lines[2]) {
 			for (let lineIndex = 2; lineIndex < lines.length; lineIndex++) { //we will use this loop to scan in the spheres
-				let line = lines[lineIndex];
+				let line = lines[lineIndex].split(/\s+/);
 				if (line) {
-					console.log(lines[lineIndex]);
+					console.log("line ot parse:",lines[lineIndex]);
+					spheres.push(new Sphere(parseInt(line[1]), parseInt(line[2]), parseInt(line[3]),	//x y z
+											parseInt(line[4]),											//radius
+											parseInt(line[5]), parseInt(line[6]), parseInt(line[7]),	//ambient rgb
+											parseInt(line[8]), parseInt(line[9]), parseInt(line[10]),	//diffuse rgb
+											parseInt(line[11]), parseInt(line[12]), parseInt(line[13]),	//specular rgb
+											parseInt(line[14])));										//shininess
 				}
-				spheres.push(new Sphere());
 			}
 		}
+		console.log(light, spheres);
 	};
+	return;
 }
